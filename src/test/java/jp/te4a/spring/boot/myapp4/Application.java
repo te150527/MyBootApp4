024bbdc5976b65df45547231e3d6ca303cf0d0eb
150527 <150527@e0601-02.JC-21.local>
2018-05-15 14:35:11 +0900

    課題5

--- src/test/java/jp/te4a/spring/boot/myapp4/Application.java
+++ src/test/java/jp/te4a/spring/boot/myapp4/Application.java
@@ -2,6 +2,8 @@ package jp.te4a.spring.boot.myapp4;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import jp.te4a.spring.boot.myapp5.MyBootApp4Application;
+
 @SpringBootApplication
 public class Application{
 	public static void main (String[] args) {
